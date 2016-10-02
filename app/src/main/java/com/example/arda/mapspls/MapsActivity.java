7f0a52712719ea59ca4d8a852874e65edd116e77
ArdaIcmez <aicmez@gmail.com>
2016-10-02 18:17:46 +0200

    Added location marker, improved object oriented approach

--- app/src/main/java/com/example/arda/mapspls/MapsActivity.java
+++ app/src/main/java/com/example/arda/mapspls/MapsActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +30,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -36,21 +38,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private LocationManager locationManager;
     private Location lastLocation;
-    private Polyline drawing;
+    private LocationListener gpsLocationListener;
 
-    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 0.5f; // 10 meters
-    private static final long MIN_TIME_BW_UPDATES = 1000*4;//1000 * 60 * 1; // 1 minute
-    private final int userColor = Color.RED;
+    // GPS Location settings
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 0.1f; // 1 meter
+    private static final long MIN_TIME_BW_UPDATES = 1000*3; // 3 seconds
+
+    //User default settings
+    private int defaultColor = Color.RED;
+    private int defaultWidth = 5;
+
+    //User personal settings
+    private UserDrawing thisUser;
+    private List<UserDrawing> drawingList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-       /* ActivityCompat.requestPermissions(this,
-                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                1);
-                */
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -58,34 +65,103 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-
-
         boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        if (!enabled) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-        }
+        if (!enabled) { promptGPS();}
 
     }
 
     //Always called after google maps initialization
-    private void initializeDraw() {
-        PolylineOptions lineOptions = new PolylineOptions().width(5).color(userColor);
-        drawing = mMap.addPolyline(lineOptions);
+    private void promptGPS() {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
     }
+    private void getGPSPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+    }
+    private void getInitLocation(){
+        if(ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            getGPSPermission();
+        }
+        else{ // Permission exists, get the starting coordonate if possible
 
+            //Puts marker on the map, enables blue dot
+            mMap.setMyLocationEnabled(true);
+
+            //Add GPS Listener to LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES, gpsLocationListener);
+
+            //Get last known location
+            lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            //Add location as the starting point of drawing, if exists
+            List<LatLng> points = thisUser.getSelfDrawing().getPoints();
+            if(lastLocation != null) {
+                points.add(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(),
+                        lastLocation.getLongitude())));
+
+            }
+            else{
+                promptGPS();
+            }
+            thisUser.setDrawingPoints(points);
+
+        }
+    }
+    private void initSelf(){
+        thisUser = new UserDrawing(5,Color.GRAY, "ArdaI");
+        PolylineOptions lineOptions = new PolylineOptions().width(thisUser.getSelfWidth())
+                .color(thisUser.getSelfColor());
+        thisUser.setSelfDrawing(mMap.addPolyline(lineOptions));
+        drawingList.add(thisUser);
+
+    }
+    private void addUser(UserDrawing drawing){
+        PolylineOptions lineOptions = new PolylineOptions().width(drawing.getSelfWidth()).
+                color(drawing.getSelfColor());
+        drawing.setSelfDrawing(mMap.addPolyline(lineOptions));
+        drawingList.add(drawing);
+    }
+    public void drawPressed(View view){
+        thisUser.setIsDrawing(!thisUser.isDrawing());
+        if(thisUser.isDrawing()){
+            PolylineOptions lineOptions = new PolylineOptions()
+                    .width(thisUser.getSelfWidth())
+                    .add(new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude()))
+                    .color(thisUser.getSelfColor());
+
+            thisUser.setSelfDrawing(mMap.addPolyline(lineOptions));
+        }
+        //TODO : Send new status to server
+    }
+    public void hidePressed(View view){
+        boolean locEnable = !mMap.isMyLocationEnabled();
+        if(ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            getGPSPermission();
+        } else{
+            mMap.setMyLocationEnabled(locEnable);
+        }
+
+        //TODO : Send new status to server
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        initializeDraw();
-        /* Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        */
+        initSelf();
 
-        LocationListener gpsLocationListener =new LocationListener(){
+        //Zoom camera
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(mMap.getMaxZoomLevel()-3);
+        mMap.animateCamera(zoom);
+
+        gpsLocationListener =new LocationListener(){
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
                 switch (status) {
@@ -103,52 +179,117 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
 
-
             public void onProviderDisabled(String provider) {
-
+                promptGPS();
             }
-
 
             public void onLocationChanged(Location location) {
-                List<LatLng> points = drawing.getPoints();
-                points.add(new LatLng(location.getLatitude(),location.getLongitude()));
-                drawing.setPoints(points);
+                if(thisUser.isDrawing()){
+                    List<LatLng> points = thisUser.getSelfDrawing().getPoints();
+                    points.add(new LatLng(location.getLatitude(),location.getLongitude()));
+                    thisUser.setDrawingPoints(points);
+                }
 
                 lastLocation = location;
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(),
-                        lastLocation.getLongitude())));
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(),
+                //        lastLocation.getLongitude())));
+
+                //TODO : Sending user location to server
+
             }
         };
+        getInitLocation();
 
-        if(ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
+    }
+    //TODO : Getting other user information from servers and drawing
+
+    /**
+     * To remove a user from the map (its line), simply do line.remove
+     */
+
+    public class UserDrawing{
+        private String nickname;
+        private int selfColor ;
+        private int selfWidth;
+        private List<Polyline> selfDrawings;
+        private Polyline selfDrawing;
+        private boolean isDrawing;
+
+        public UserDrawing(int width,int color, String nickname ){
+            this.nickname = nickname;
+            this.selfColor = color;
+            this.selfWidth = width;
+            this.selfDrawings = new ArrayList<>();
+            this.isDrawing = true;
         }
-        else{
-            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-            {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
-                        MIN_DISTANCE_CHANGE_FOR_UPDATES, gpsLocationListener);
-                lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                List<LatLng> points = drawing.getPoints();
-                if(lastLocation != null) {
-                    points.add(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(),
-                            lastLocation.getLongitude())));
+        public int getSelfColor() {
+            return selfColor;
+        }
 
+        public void setSelfColor(int selfColor) {
+            this.selfColor = selfColor;
+        }
+
+        public int getSelfWidth() {
+            return selfWidth;
+        }
+
+        public void setSelfWidth(int selfWidth) {
+            this.selfWidth = selfWidth;
+        }
+
+        public Polyline getSelfDrawing() {
+            return selfDrawing;
+        }
+
+        public void setSelfDrawing (Polyline selfDrawing) {
+            this.selfDrawing =  selfDrawing;
+            this.selfDrawings.add(selfDrawing);
+        }
+
+        public void setDrawingPoints(List<LatLng> points) {
+            this.selfDrawing.setPoints(points);
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public boolean isDrawing() {
+            return isDrawing;
+        }
+
+        public void setIsDrawing(boolean isDrawing) {
+            this.isDrawing = isDrawing;
+        }
+
+        public List<Polyline> getSelfDrawings() {
+            return selfDrawings;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                String permission = permissions[i];
+                int grantResult = grantResults[i];
+
+                if (permission.equals(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        getInitLocation();
+                    } else {
+                        getGPSPermission();
+                    }
                 }
-                drawing.setPoints(points);
-                CameraUpdate zoom=CameraUpdateFactory.zoomTo(mMap.getMaxZoomLevel()-4);
-                mMap.animateCamera(zoom);
-            }
-            else{
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
             }
         }
-
     }
 }
